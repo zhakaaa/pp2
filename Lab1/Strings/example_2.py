84a24e20d10789b3c5391c736e07563efe96280c
zhakaaa <zhaka9072@gmail.com>
2024-01-24 15:13:04 +0600

    some changes

---
+++ Lab1/Strings/example_2.py
@@ -0,0 +1,14 @@
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
